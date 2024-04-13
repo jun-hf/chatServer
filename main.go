0ecb2e5951a10674151f55209beec766d5fc8da7
jun-hf <fong.junh@gmail.com>
2024-04-13 16:47:16 +0100

    create client connection

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+type client chan<- string
+
+var (
+	entering = make(chan client)
+	leaving = make(chan client)
+	messages = make(chan string) // all incoming client message
+)
+
+
 
 func main() {
-	fmt.Println("Hello world")
+	listen, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go broadcaster()
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection %v", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(con net.Conn) {
+	fmt.Fprintln(con, "Hello")
 }
